df0f105160fa859a9a4f6fdb3b8631120a5af944
jporras1 <javi.jr2150@gmail.com>
2019-10-16 22:44:53 -0500

    Finished the chain animations. Which is essentially an animation and for the completion handler we add another animation. We do this twice, once for the the titleLabel and once for the bodyLabel.

--- chainingAnimations/ViewController.swift
+++ chainingAnimations/ViewController.swift
@@ -9,13 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground(as: .yellow)
+        setupLabels()
+        setupStackView()
         
+        //Fun animation
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapAnimation)))
     }
-
+    
+    let titleLabel = UILabel()
+    let bodyLabel = UILabel()
+    
+    fileprivate func setupLabels() {
+        titleLabel.backgroundColor = .red
+        bodyLabel.backgroundColor = .green
+        
+        titleLabel.text = "Welcome to Company XYZ"
+        titleLabel.font = UIFont(name: "Futura", size: 34)
+        titleLabel.numberOfLines = 0
+        
+        bodyLabel.text = "This is a lot of text, that should span across multiple lines. It is a lot of text"
+        bodyLabel.numberOfLines = 0
+    }
+    
+    fileprivate func setupStackView() {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        let padding: CGFloat = -100
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: padding).isActive = true
+    }
+    
+    @objc
+    func handleTapAnimation(){
+        print("animating")
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.titleLabel.alpha = 0
+                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -150)
+            }, completion: nil)
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.bodyLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.bodyLabel.alpha = 0
+                self.bodyLabel.transform = self.bodyLabel.transform.translatedBy(x: 0, y: -150)
+            }, completion: nil)
+        }
+        
+        //UIView.animate(withDuration: <#T##TimeInterval#>, delay: <#T##TimeInterval#>, usingSpringWithDamping: <#T##CGFloat#>, initialSpringVelocity: <#T##CGFloat#>, options: <#T##UIView.AnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+    }
 
 }
 
@@ -24,3 +79,8 @@ extension UIViewController{
         self.view.backgroundColor = color
     }
 }
+
+
+////was inside viewcontroller
+
+//stackView.frame = CGRect(x: 0, y: 0, width: 200, height: 400)
